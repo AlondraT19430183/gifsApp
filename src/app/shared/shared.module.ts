1a436e8634b3994898931bd2436459e4c76e77f9
Alondra Treviño <114121262+AlondraT19430183@users.noreply.github.com>
2022-10-05 21:04:25 -0500

    mcoos
    sdsd

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SlidebarComponent } from './slidebar/slidebar.component';
+
+
+
+@NgModule({
+  declarations: [
+    SlidebarComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+
+  exports:[
+    SlidebarComponent 
+  ]
+})
+export class SharedModule { }
